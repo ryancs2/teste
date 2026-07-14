474fd59f955ff3ba609876907ae4704aba8595b2
rytox <rs2421183@gmail.com>
2026-07-13 21:44:29 -0300

    aula 07.13 com CRUD

--- meusite/tarefas/urls.py
+++ meusite/tarefas/urls.py
@@ -5,6 +5,5 @@ app_name = 'tarefas'
 urlpatterns = [
     path('', views.tarefas_home, name='home'),
     path('adicionar/', views.tarefas_adicionar, name='adicionar'),
-    path('editar/', views.tarefas_editar,name='editar'),
-    path('excluir/', views.tarefas_excluir,name='excluir'),
+    path('remover/<int:id>', views.tarefas_remover, name='remover')
 ]
--- meusite/tarefas/views.py
+++ meusite/tarefas/views.py
@@ -1,18 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import TarefaForm
+from .models import TarefaModel
 
 # Create your views here.
 
 def tarefas_home(request):
     contexto = {
-        'nome': 'Ryan'
+        'nome': 'Ryan',
+        'tarefas': TarefaModel.objects.all()
     }
     return render(request, 'pagetarefas/home.html', contexto)
 
 def tarefas_adicionar(request):
-    return render(request, 'pagetarefas/adicionar.html')
+    if request.method == "POST":
+        formulario = TarefaForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect("tarefas:home")
+    contexto = {
+        'form': TarefaForm()
+    }
 
-def tarefas_editar(request):
-    return render(request, 'pagetarefas/editar.html')
+    return render(request, 'pagetarefas/adicionar.html', contexto)
 
-def tarefas_excluir(request):
-    return render(request, 'pagetarefas/excluir.html')
+def tarefas_remover(request,id):
+    tarefa = get_object_or_404(TarefaModel, id=id)
+    tarefa.delete()
+    return redirect("tarefas:home")
